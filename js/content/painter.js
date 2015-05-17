ac7567b036bed3f01728e531474968f1b38aad0d
Cole119 <10chandlerc@gmail.com>
2015-05-16 20:48:29 -0400

    Smoothed the drawn gestures

--- js/content/painter.js
+++ js/content/painter.js
@@ -5,23 +5,68 @@ function Painter() {
     var previewDiv;
     var canvas;
     var ctx;
+    var lastX, lastY;
+    var points = [];
+
+    function getMidpoint(p1, p2) {
+        return {
+            x: (p1.x + p2.x) / 2,
+            y: (p1.y + p2.y) / 2
+        };
+    }
+
+    function wipeCanvas() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    function initializeCanvas() {
+        var canvas = document.createElement("canvas");
+        canvas.id = "gesture-canvas";
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        return canvas;
+    }
+
+    function drawCurve() {
+        // We need at least 3 points to make a smooth curve.
+        if (points.length < 3) {
+            return;
+        }
+
+        wipeCanvas();
+
+        ctx.beginPath();
+        // Move the 'pencil' to the first point
+        ctx.moveTo(points[0].x, points[0].y);
+
+        for (var i = 1; i < points.length - 2; i++) {
+            var midpoint = getMidpoint(points[i], points[i+1]);
+            ctx.quadraticCurveTo(points[i].x, points[i].y, midpoint.x, midpoint.y);
+        }
+
+        // curve through the last two points
+        ctx.quadraticCurveTo(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
+
+        ctx.stroke();
+    }
 
     this.drawGesture = function(x, y) {
-        if (!document.getElementById("gesture-canvas")) {
-            canvas = document.createElement("canvas");
-            canvas.id = "gesture-canvas";
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
+        points.push({x: x, y: y});
+
+        if (!canvas) {
+            canvas = initializeCanvas();
             (document.body || document.documentElement).appendChild(canvas);
 
             ctx = canvas.getContext('2d');
-            ctx.strokeStyle = "rgb(18,89,199)";
-            ctx.beginPath();
-            ctx.moveTo(x, y);
+            ctx.strokeStyle = 'rgb(18,89,199)';
+            ctx.translate(0.5, 0.5);
+            ctx.lineJoin = 'round';
+            ctx.lineCap = 'round';
+            ctx.lineWidth = 3;
         }
 
-        ctx.lineTo(x, y);
-        ctx.stroke();
+        drawCurve();
     };
 
     this.updateDirectionText = function(text) {
@@ -63,5 +108,7 @@ function Painter() {
             directionContainer.parentNode.removeChild(directionContainer);
             directionContainer = undefined;
         }
+
+        points = [];
     };
 }
